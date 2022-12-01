4c411367ef905f194595cbd96d5f3755b67545d5
harshadbadre <harshad.badre@coditas.com>
2022-12-01 15:20:07 +0530

    handle blutooth permission

--- app/src/androidTest/java/com/example/allpermisionautomation/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/allpermisionautomation/ExampleInstrumentedTest.kt
@@ -1,12 +1,16 @@
 package com.example.allpermisionautomation
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.filters.LargeTest
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.By
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.UiSelector
+import org.junit.Assert.*
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,11 +18,28 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
+@LargeTest
 class ExampleInstrumentedTest {
+
+    /**
+     UiDevice class use to access and perform operations device on which app is runnning
+     */
+    private var device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+
+    //launch the app
+    @get:Rule
+    var activityScenarioRule= ActivityScenarioRule(MainActivity::class.java)
+
+
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.allpermisionautomation", appContext.packageName)
+    fun clickonButton() {
+        //click on blutooth button
+        device.findObject(UiSelector().text("BLUETOOTH")).click()
+        //click on allow
+        device.findObject(UiSelector().text("Allow")).click()
+        //click home button
+        device.pressHome()
+
     }
 }
